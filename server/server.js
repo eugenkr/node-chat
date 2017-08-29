8fafa58e9f2ef8ef2167773a1758610ae2e69c92
Yevgen Kruglyk <eugen.kr@gmail.com>
2017-08-29 15:37:53 +0300

    greet new user

--- server/server.js
+++ server/server.js
@@ -14,14 +14,34 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected!');
     
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Wellcome to chat!',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined the chat!',
+    createdAt: new Date().getTime()
+  });
+  
   socket.on('createMessage', (data) => {
     console.log('Create message: ', data);
 
+    //  sends to every connection
     io.emit('newMessage', {
       from: data.from,
       text: data.text,
       createdAt: new Date().getTime()
     });
+
+    // sends to every connection but this one
+    // socket.broadcast.emit('newMessage', {
+    //   from: data.from,
+    //   text: data.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
